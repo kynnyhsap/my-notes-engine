82fd8cd4a4f70a4824484669f9287d934966b646
kynnyhsap <tobirawork@gmail.com>
2023-02-04 14:23:33 +0200

    init

---
+++ netlify/functions/hello.ts
@@ -0,0 +1,12 @@
+import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+
+const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+    // your server-side functionality
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Hello World" }),
+    }
+};
+
+export { handler };
