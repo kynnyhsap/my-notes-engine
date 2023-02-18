3057ddad137bbe746249d79e1ba8183522ca418e
kynnyhsap <tobirawork@gmail.com>
2023-02-18 16:52:36 +0200

    wip

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,18 @@ import "https://deno.land/std@0.177.0/dotenv/load.ts";
 import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 
 const router = new Router();
+
 router.get("/", (ctx) => {
   ctx.response.body = "Hello world!";
+});
+
+router.post("/podcast-note", async (ctx) => {
+  const data = await ctx.request.body({ type: "json" }).value;
+
+  console.log({ data });
+
+  ctx.response.body = { data };
+  ctx.response.status = 200;
 });
 
 const app = new Application();
