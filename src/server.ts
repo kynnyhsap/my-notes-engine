066f8eb17ee70da2b59ad7a55d9c1065bbac8bf2
kynnyhsap <tobirawork@gmail.com>
2023-02-18 17:14:52 +0200

    wip

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,59 @@
 import "https://deno.land/std@0.177.0/dotenv/load.ts";
 import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
+import { getXataClient } from "./xata.ts";
+
+interface MyPodcastNote {
+  podcastTitle: string;
+
+  episodeTitle: string;
+
+  text: string;
+}
 
 const router = new Router();
 
 router.get("/", (ctx) => {
-  ctx.response.body = "Hello world!";
+  ctx.response.body = { message: "Oh, so good to be alive!" };
 });
 
 router.post("/podcast-note", async (ctx) => {
-  const data = await ctx.request.body({ type: "json" }).value;
+  const xata = getXataClient();
+  const data: MyPodcastNote = await ctx.request.body({ type: "json" }).value;
 
-  console.log({ data });
+  console.log("Received note:", data);
 
-  ctx.response.body = { data };
+  let podcast = await xata.db.Podcasts.filter({
+    title: data.podcastTitle,
+  }).getFirst();
+  if (!podcast) {
+    podcast = await xata.db.Podcasts.create({ title: data.podcastTitle });
+  }
+
+  let podcastEpisode = await xata.db.PodcastEpisodes.filter({
+    title: data.episodeTitle,
+  }).getFirst();
+  if (!podcastEpisode) {
+    podcastEpisode = await xata.db.PodcastEpisodes.create({
+      title: data.episodeTitle,
+      podcast: podcast.id,
+    });
+  }
+
+  const podcastEpisodeNote = await xata.db.PodcastEpisodeNotes.create({
+    text: data.text,
+    podcastEpisode: podcastEpisode.id,
+  });
+
+  console.log("Saved note:", podcastEpisodeNote);
+
+  ctx.response.body = podcastEpisodeNote;
   ctx.response.status = 200;
 });
 
 const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
+
+app.addEventListener("listen", () => console.log("Up and running!"));
 
 await app.listen({ port: 8080 });
